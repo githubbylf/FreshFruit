21b03dabe250d5d4a13036e92d86eb8c44a43d98
Asa <auth_account@outlook.com>
2015-09-06 17:39:45 +0800

    ADD ENUMS

--- FreshFruit.Common/CommonUtils/BaseEnums.cs
+++ FreshFruit.Common/CommonUtils/BaseEnums.cs
@@ -6,24 +6,116 @@ using System.Threading.Tasks;
 
 namespace FreshFruit.Common.CommonUtils
 {
-    class BaseEnums
-    {
-
-    }
 
     /// <summary>
-    /// Emun Template
+    /// 订单状态
     /// </summary>
-    public enum EmunTemplate
+    public enum ORDER_STATUS
     {
         /// <summary>
-        /// Emun Template T1
+        /// 未确认
         /// </summary>
-        T1 = 1,
+        NEW_STATUS = 0,
 
         /// <summary>
-        /// Emun Template T2
+        /// 已确认
         /// </summary>
-        T2 = 2
+        CONFIRM_STATUS = 1,
+
+        /// <summary>
+        ///已取消
+        /// </summary>
+        CANCEL_STATUS = 2,
+
+        /// <summary>
+        /// 无效
+        /// </summary>
+        INVALID_STATUS = 3,
+
+        /// <summary>
+        /// 退货
+        /// </summary>
+        RETURNED_STATUS = 4,
+
+        /// <summary>
+        /// 预售
+        /// </summary>
+        BOOK_STATUS = 5,
+
+        /// <summary>
+        /// 部分退货
+        /// </summary>
+        RETURNED_SOMERETURN = 6,
+
+        /// <summary>
+        /// 预付
+        /// </summary>
+        PRE_STATUS = 7
     }
+
+
+    /// <summary>
+    /// 支付状态
+    /// </summary>
+    public enum ORDER_PAY_STATUS
+    {
+        /// <summary>
+        /// 未付款
+        /// </summary>
+        NOT_PAY = 0,
+
+        /// <summary>
+        /// 付款中
+        /// </summary>
+        PAYING = 1,
+
+        /// <summary>
+        /// 已付款
+        /// </summary>
+        PAYED = 2,
+
+        /// <summary>
+        /// 付款错误
+        /// </summary>
+        PAY_ERR = 3,
+
+        /// <summary>
+        /// 部分付款
+        /// </summary>
+        PAY_PART = 4
+    }
+
+
+    /// <summary>
+    /// 发货状态
+    /// </summary>
+    public enum SHIPPING_STATUS
+    {
+        /// <summary>
+        /// 未发货
+        /// </summary>
+        SHIPPING_NOT_SEND = 0,
+
+        /// <summary>
+        /// 备货中
+        /// </summary>
+        SHIPPING_PREPARE_GOODS = 1,
+
+        /// <summary>
+        /// 已出库
+        /// </summary>
+        SHIPPING_OUT = 2,
+
+        /// <summary>
+        /// 已发货
+        /// </summary>
+        SHIPPING_SEND = 3,
+
+        /// <summary>
+        /// 已收货
+        /// </summary>
+        SHIPPING_RECEIVED = 4
+
+    }
+
 }
---
+++ FreshFruit.Common/ToolsUtils/HexUtils.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreshFruit.Common.ToolsUtils
+{
+    public class HexUtils
+    {
+//        int d = 10;
+
+////十进制转二进制字符串
+// Console.WriteLine(Convert.ToString(d,2));
+////输出: 1010
+
+
+////十进制转十六进制字符串
+//Console.WriteLine(Convert.ToString(d,16));
+////输出: a
+
+////二进制字符串转十进制数
+//string bin = "1010";
+//Console.WriteLine(Convert.ToInt32(bin,2));
+////输出: 10
+
+////二进制字符串转十六进制数
+//string bin = "1010";
+//Console.WriteLine(string.Format("{0:x}",Convert.ToInt32(bin,2));
+////输出: a
+
+////十六进制转二进制字符串
+//Console.WriteLine(Convert.ToString(0xa,2));
+////输出: 1010
+
+////十六进制转十进制数
+//Console.WriteLine(Convert.ToString(0xa,10));
+////输出: 10
+
+    }
+}
